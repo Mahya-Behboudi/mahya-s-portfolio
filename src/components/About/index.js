a8b6c08c87968e54e34920be3c1e844ff9ad72c9
Mahya beh <mahyab79@gmail.com>
2022-09-05 15:51:10 +0430

    finished all the about and home pages

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,8 @@ import { faReact , faHtml5 ,faCss3 ,fa, faJsSquare, faGit ,faPython} from '@fort
 
 const About = () => {
     return (
-        <div className='container about-page'>
+       <>
+             <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
                     <AnimatedLetters
@@ -51,6 +52,7 @@ const About = () => {
                 </div>
             </div>
         </div>
+       </>
     );
 };
 
